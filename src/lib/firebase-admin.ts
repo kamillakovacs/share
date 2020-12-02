1137df2926f58d15e9f163bb37a11c1a489b08aa
Kovács Kamilla <kovacs.kamilla@arteries.hu>
2020-12-02 09:01:52 +0100

    try admin with env

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,12 @@
 import * as firebaseAdmin from "firebase-admin";
-import * as serviceAccount from "../../share-service-account.json";
+// import * as serviceAccount from "../../share-service-account.json";
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: serviceAccount.private_key,
-      clientEmail: serviceAccount.client_email,
-      projectId: serviceAccount.project_id,
+      privateKey: process.env.PRIVATE_KEY,
+      clientEmail: process.env.CLIENT_EMAIL,
+      projectId: process.env.PROJECT_ID,
     }),
     databaseURL: "https://sharespareservation.firebaseio.com",
   });
