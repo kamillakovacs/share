7ce40b0aefa70796b879467756db936d549b2fed
Kamilla Kovacs <knkovacs@gmail.com>
2022-02-27 13:31:45 +0100

    add decryption

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,26 @@
 import * as firebase from "firebase-admin";
+import crypto from "crypto";
+import service from "../../service-account.enc";
 
-// Fetch the service account key JSON file contents
-var serviceAccount = require("../../service-account.json");
-// Initialize the app with a service account, granting admin privileges
+const algorithm = "aes-128-cbc";
+const decipher = crypto.createDecipheriv(
+  algorithm,
+  process.env.SERVICE_ENCRYPTION_KEY,
+  process.env.SERVICE_ENCRYPTION_IV
+);
+let decrypted = decipher.update(service.encrypted, "base64", "utf8");
+decrypted += decipher.final("utf8");
+
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
-  credential: firebase.credential.cert(serviceAccount),
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  credential: firebase.credential.cert(JSON.parse(decrypted)),
   databaseAuthVariableOverride: {
     uid: "share-service-worker",
   },
