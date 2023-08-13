158930e3e7bc205c76dbd0f728b1b2db4aee8af0
Zsirai-Kovács Kamilla <knkovacs@gmail.com>
2023-08-13 20:33:14 +0200

    upgrades and hungarian

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,6 @@ const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "en-US",
-    locales: ["en-US", "hu-HU"],
-    localePath: path.resolve("./public/locales"),
+    locales: ["en-US", "hu-HU"]
   },
 };
